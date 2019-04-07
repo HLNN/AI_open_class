a3289c06180d7ed7a48da02b59f71304be90bc6b
HLN <huangln555@gmail.com>
2019-04-07 12:16:52 +0800

    Update week4-5/test_CNN.py

--- week4-5/test_CNN.py
+++ week4-5/test_CNN.py
@@ -6,32 +6,38 @@ import os
 # import cv2
 
 
-cnn = torch.load('net.pkl')
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                16, 32, 5, 1, 2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.out = nn.Linear(32*7*7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output
+
+
+cnn = CNN()
+cnn.load_state_dict(torch.load('./model/cnn_params_gpu_100_100_0.001.pkl'))
 print(cnn)
-
-
-optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
-loss_func = nn.CrossEntropyLoss()
-
-
-for epoch in range(EPOCH):
-    for step, (b_x, b_y) in enumerate(train_loader):
-        output = cnn(b_x)
-        loss = loss_func(output, b_y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if step % 50 == 0:
-            for s, (b_x, b_y) in enumerate(test_loader):
-                test_output = cnn(b_x)
-                pred_y = torch.max(test_output, 1)[1].data.numpy()
-                accuracy = float((pred_y == b_y.data.numpy()).astype(int).sum()) / float(b_y.size(0))
-                print('Epoch: ', epoch, '| Steps: %03d' % step,  '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.4f' % accuracy)
-                break
-
-
-
-
-
 
