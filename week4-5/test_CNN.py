e979acced944589f4bf45ab9d3727af205e136ee
HLN <huangln555@gmail.com>
2019-04-11 23:33:33 +0800

    Update week4-5 test_CNN.py

--- week4-5/test_CNN.py
+++ week4-5/test_CNN.py
@@ -4,6 +4,7 @@ import torch.utils.data as DATA
 import torchvision
 import os
 import cv2
+import math
 
 
 class CNN(nn.Module):
@@ -37,21 +38,64 @@ class CNN(nn.Module):
         return output
 
 
-cnn = CNN()
-cnn.load_state_dict(torch.load('./model/cnn_params_gpu_100_100_0.001.pkl'))
-print(cnn)
+class MNIST:
+    def __init__(self):
+        self.predict = False
+        self.cnn = CNN()
+        self.cnn.load_state_dict(torch.load('./model/cnn_params_gpu_100_100_0.001.pkl'))
+        self.src = cv2.imread("5.jpg")
+        self.p1 = ()
+        self.p2 = ()
+
+        self.src = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
+        if self.src.shape[0] > 1080 or self.src.shape[1] > 1920:
+            factor = max(math.ceil(self.src.shape[0] / 1080), math.ceil(self.src.shape[1] / 1920))
+            self.src = cv2.resize(self.src, (int(self.src.shape[0] / factor), int(self.src.shape[1] / factor)))
+
+    def show(self):
+        if self.p1 and self.p2:
+            img = self.src.copy()
+            cv2.imshow("MNIST", cv2.rectangle(img, self.p1, self.p2, (0, 0, 0), 2))
+        else:
+            cv2.imshow("MNIST", self.src)
+
+    def mouseCallback(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.p1 = (x, y)
+            self.p2 = ()
+
+        elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
+            self.p2 = (x, y)
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            if self.p1 and self.p2:
+                self.predict = True
+            else:
+                self.p1 = self.p2 = ()
+
+    def main(self):
+        print(self.cnn)
+        print("Load successfully, waiting...")
+        cv2.imshow("MNIST", self.src)
+        cv2.setMouseCallback("MNIST", self.mouseCallback)
+        while True:
+            self.show()
+            cv2.waitKey(30)
+            if self.predict:
+                src = self.src[self.p1[0]:self.p2[0], self.p1[1]:self.p2[1]]
+                src = cv2.resize(src, (28, 28))
+                thresh, src = cv2.threshold(src, 127, 255, cv2.THRESH_BINARY_INV)
+
+                test_input = torch.from_numpy(src).float()
+                test_input = torch.unsqueeze(test_input, 0)
+                test_input = torch.unsqueeze(test_input, 0)
+                test_output = self.cnn(test_input)
+                pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
+                print("The number in picture is ", pred_y)
+                self.predict = False
 
 
-src = cv2.imread("5.jpg")
-src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-src = cv2.resize(src, (28, 28))
-thresh, src = cv2.threshold(src, 127, 255, cv2.THRESH_BINARY_INV)
-cv2.imshow("", src)
-
-test_input = torch.from_numpy(src).float()
-test_input = torch.unsqueeze(test_input, 0)
-test_input = torch.unsqueeze(test_input, 0)
-test_output = cnn(test_input)
-pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
-print("The number in picture is ", pred_y)
+if __name__ == '__main__':
+    mnist = MNIST()
+    mnist.main()
 
