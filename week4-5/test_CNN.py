3cd424ffcdc5f53c81be4179613f79734f3e5e15
HLN <huangln555@gmail.com>
2019-04-13 13:18:28 +0800

    Update week4-5 test_CNN.py

--- week4-5/test_CNN.py
+++ week4-5/test_CNN.py
@@ -42,15 +42,18 @@ class MNIST:
     def __init__(self):
         self.predict = False
         self.cnn = CNN()
-        self.cnn.load_state_dict(torch.load('./model/cnn_params_gpu_100_100_0.001.pkl'))
-        self.src = cv2.imread("5.jpg")
+        self.src = cv2.imread("number.jpg")
         self.p1 = ()
         self.p2 = ()
 
+        if torch.cuda.is_available():
+            self.cnn.load_state_dict(torch.load('./model/cnn_params_gpu_100_100_0.001.pkl'))
+        else:
+            self.cnn.load_state_dict(torch.load('./model/cnn_params_gpu_100_100_0.001.pkl', map_location='cpu'))
         self.src = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
         if self.src.shape[0] > 1080 or self.src.shape[1] > 1920:
-            factor = max(math.ceil(self.src.shape[0] / 1080), math.ceil(self.src.shape[1] / 1920))
-            self.src = cv2.resize(self.src, (int(self.src.shape[0] / factor), int(self.src.shape[1] / factor)))
+            factor = max(math.ceil(self.src.shape[1] / 1080), math.ceil(self.src.shape[0] / 1920))
+            self.src = cv2.resize(self.src, (int(self.src.shape[1] / factor), int(self.src.shape[0] / factor)))
 
     def show(self):
         if self.p1 and self.p2:
@@ -68,7 +71,7 @@ class MNIST:
             self.p2 = (x, y)
 
         elif event == cv2.EVENT_LBUTTONUP:
-            if self.p1 and self.p2:
+            if self.p1 and self.p2 and self.p1[0] < self.p2[0] and self.p1[1] < self.p2[1]:
                 self.predict = True
             else:
                 self.p1 = self.p2 = ()
@@ -82,7 +85,7 @@ class MNIST:
             self.show()
             cv2.waitKey(30)
             if self.predict:
-                src = self.src[self.p1[0]:self.p2[0], self.p1[1]:self.p2[1]]
+                src = self.src[self.p1[1]:self.p2[1], self.p1[0]:self.p2[0]]
                 src = cv2.resize(src, (28, 28))
                 thresh, src = cv2.threshold(src, 127, 255, cv2.THRESH_BINARY_INV)
 
